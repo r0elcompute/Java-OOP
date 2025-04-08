46bb7fa1caee9486f8f5693f6ec449e5a090688c
r0elcompute <rodgersmwangi20@gmail.com>
2025-04-08 16:34:56 +0300

    Modified parameterized constructors

--- Constructors/Types-of-constructors/parameterized-constructor/Student.java
+++ Constructors/Types-of-constructors/parameterized-constructor/Student.java
@@ -13,4 +13,12 @@ public class Student {
         this.name = name;
         this.regno = regno;
     }
+
+    public static void main(String[] args) {
+        //create an object using the new keyword
+        Student student = new Student(20, "Rodgers", "BTIT/088J/2024");
+        System.out.println("Age: " + student.age);
+        System.out.println("Name: " + student.name);
+        System.out.println("Registration Number: " + student.regno);
+    }
 }
