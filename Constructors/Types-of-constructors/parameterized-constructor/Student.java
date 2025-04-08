3f7122d599e78eaac3513901e75061bc17312c04
r0elcompute <rodgersmwangi20@gmail.com>
2025-04-08 16:29:38 +0300

    modified parameterized constructors

---
+++ Constructors/Types-of-constructors/parameterized-constructor/Student.java
@@ -0,0 +1,16 @@
+//This code snippet illustrates parameterized constructors
+
+public class Student {
+    public int age;
+    public String name;
+    public String regno;
+
+    //parameterized constructor
+    //It takes parameters 
+
+    Student(int age, String name, String regno) {
+        this.age = age;
+        this.name = name;
+        this.regno = regno;
+    }
+}
